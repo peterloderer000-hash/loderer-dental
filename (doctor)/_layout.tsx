ee2bb74ee34fde2ecf54ce4c026d39f59631c61f
Peter Loderer <tvoj@email.com>
2026-06-02 07:26:21 +0200

    chore: remove .clean backups and root-level duplicates

--- (doctor)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function DoctorLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Doktor Dashboard', headerShown: false }} />
-    </Stack>
-  );
-}
